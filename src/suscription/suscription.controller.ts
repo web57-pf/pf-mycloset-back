9688f3cc777d2c4f41c49b96aff9f0a25145f417
Maximo Stefanutti <stefanuttimaximo@gmail.com>
2025-02-21 16:37:00 -0300

    suscription API

--- src/suscription/suscription.controller.ts
+++ src/suscription/suscription.controller.ts
@@ -10,26 +10,54 @@ import {
 import { SuscriptionService } from './suscription.service';
 import { CreateSuscriptionDto } from './dto/create-suscription.dto';
 import { UpdateSuscriptionDto } from './dto/update-suscription.dto';
-
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 @Controller('suscription')
+@ApiTags('suscription')
 export class SuscriptionController {
   constructor(private readonly suscriptionService: SuscriptionService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create subscription type' })
+  @ApiBody({
+    description: 'Creates subscription type by body',
+    examples: {
+      SubscriptionType: {
+        value: {
+          name: '',
+          price: 0,
+          duration: 0,
+        },
+      },
+    },
+  })
   createSubscriptionType(@Body() createSuscriptionDto: CreateSuscriptionDto) {
     return this.suscriptionService.createSubscriptionType(createSuscriptionDto);
   }
 
+  @ApiOperation({ summary: 'Get all subscription types' })
   @Get()
   getAllSubscriptionTypes() {
     return this.suscriptionService.getAllSubscriptionTypes();
   }
-
+  @ApiOperation({ summary: 'Get subscription type by id' })
   @Get(':id')
   getSubscriptionTypeById(@Param('id') id: string) {
     return this.suscriptionService.getSubscriptionTypeById(id);
   }
-
+  @ApiOperation({ summary: 'Update subscription type by id and body' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        name: { type: 'string' },
+        price: { type: 'number' },
+        duration: { type: 'number' },
+      },
+    },
+    examples: {
+      SubscriptionType: { value: { name: '', price: 0, duration: 0 } },
+    },
+  })
   @Put(':id')
   updateSubscriptionType(
     @Param('id') id: string,
@@ -40,7 +68,7 @@ export class SuscriptionController {
       updateSuscriptionDto,
     );
   }
-
+  @ApiOperation({ summary: 'Delete subscription type by id' })
   @Delete(':id')
   deleteSubscriptionType(@Param('id') id: string) {
     return this.suscriptionService.deleteSubscriptionType(id);
