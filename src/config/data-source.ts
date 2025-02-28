60539040506d06d8f78df88e811d17c095b9bf83
Alejandro Luna <alejandro2002.uar@gmail.com>
2025-02-27 23:07:13 -0300

    Enable SSL with rejectUnauthorized option in data source configuration

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -13,9 +13,9 @@ const PostgresDataSourceOptions: DataSourceOptions = {
     username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
-    ssl: false, //{
-    // rejectUnauthorized: true ,
-    // } ,
+    ssl: {
+     rejectUnauthorized: true ,
+    } ,
     entities: ['dist/**/*.entity{.ts,.js}'],
     synchronize: true,
     logging: false,
