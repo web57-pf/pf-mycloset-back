506cd4d59966e812f63345066f44c58a11712afe
Alejandro Luna <alejandro2002.uar@gmail.com>
2025-03-02 18:34:02 -0300

    Correcion bug

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -6,10 +6,17 @@ import { User } from 'src/users/entities/user.entity';
 import { OrderDetail } from 'src/order_detail/entities/order_detail.entity'
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MercadopagoService } from 'src/payment/mercadopago/mercadopago/mercadopago.service';
+import { JwtModule } from '@nestjs/jwt';
+import { contanst } from 'src/auth/jwt.contanst';
 
 @Module({
   imports: [
+    JwtModule.register({
+          secret: contanst.secret,
+          signOptions:{ expiresIn: '1h'}
+    }),
     TypeOrmModule.forFeature([Order, User, OrderDetail]),
+    
   ],
   controllers: [OrderController],
   providers: [OrderService, MercadopagoService],
