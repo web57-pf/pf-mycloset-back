d4cc8302c896257b09d283ea401718e8401e3a4c
Maximo Stefanutti <stefanuttimaximo@gmail.com>
2025-03-09 19:50:56 -0300

    delete email

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -30,14 +30,14 @@ export class AuthController {
   })
   @Post('signup')
   async signup(@Body() data: SignupDTO) {
-    const emailDto: CreateEmailDto = {
-      email: data.email, // Asegurar que 'to' sea un array
-      subject: 'Bienvenido a nuestra plataforma',
-      html: `<p>Hola ${data.name}, gracias por registrarte. Confirma tu correo electrónico.</p>`,
-      type: 'confirmation',
-    };
+    // const emailDto: CreateEmailDto = {
+    //   email: data.email,
+    //   subject: 'Bienvenido a nuestra plataforma',
+    //   html: `<p>Hola ${data.name}, gracias por registrarte. Confirma tu correo electrónico.</p>`,
+    //   type: 'confirmation',
+    // };
 
-    await this.emailService.sendConfirmationEmail(emailDto); // Pasamos un CreateEmailDto válido
+    // await this.emailService.sendEmailWelcome(emailDto); // Pasamos un CreateEmailDto válido
 
     return await this.authService.signupServices(data);
   }
