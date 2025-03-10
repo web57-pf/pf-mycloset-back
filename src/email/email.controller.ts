122001ccb41cc13d052793300f6f4c30c378e498
Maximo Stefanutti <stefanuttimaximo@gmail.com>
2025-03-10 12:47:41 -0300

    sin controller de email

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -1,42 +1,42 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { EmailService } from './email.service';
-import { CreateEmailDto } from './dto/create-email.dto';
-import { ApiOperation } from '@nestjs/swagger';
+// import { Body, Controller, Post } from '@nestjs/common';
+// import { EmailService } from './email.service';
+// import { CreateEmailDto } from './dto/create-email.dto';
+// import { ApiOperation } from '@nestjs/swagger';
 
-@Controller('email')
-export class EmailController {
-  constructor(private readonly emailService: EmailService) {}
+// @Controller('email')
+// export class EmailController {
+//   constructor(private readonly emailService: EmailService) {}
 
-  @ApiOperation({ summary: 'Enviar email' })
-  @Post('send')
-  async sendEmail(@Body() dto: CreateEmailDto) {
-    console.log('dto', dto);
+//   @ApiOperation({ summary: 'Enviar email' })
+//   @Post('send')
+//   async sendEmail(@Body() dto: CreateEmailDto) {
+//     console.log('dto', dto);
 
-    switch (dto.type) {
-      case 'confirmation':
-        await this.emailService.sendConfirmationEmail(dto);
-        break;
-      case 'welcome':
-        await this.emailService.sendEmailWelcome(dto);
-        break;
-      case 'reset.password':
-        await this.emailService.sendEmailResetPassword(dto);
-        break;
-      case 'notification':
-        await this.emailService.sendEmailNotification(dto);
-        break;
-      case 'confirm':
-        await this.emailService.sendEmailConfirmSubscription(dto);
-        break;
-      case 'subscription.confirm':
-        await this.emailService.sendEmailSubscription(dto);
-        break;
-      case 'cancel.subscription':
-        await this.emailService.sendEmailCancellation(dto);
-        break;
-      default:
-        throw new Error('Invalid email type');
-    }
-    return { message: 'Email sent' };
-  }
-}
+//     switch (dto.type) {
+//       case 'confirmation':
+//         await this.emailService.sendConfirmationEmail(dto);
+//         break;
+//       case 'welcome':
+//         await this.emailService.sendEmailWelcome(dto);
+//         break;
+//       case 'reset.password':
+//         await this.emailService.sendEmailResetPassword(dto);
+//         break;
+//       case 'notification':
+//         await this.emailService.sendEmailNotification(dto);
+//         break;
+//       case 'confirm':
+//         await this.emailService.sendEmailConfirmSubscription(dto);
+//         break;
+//       case 'subscription.confirm':
+//         await this.emailService.sendEmailSubscription(dto);
+//         break;
+//       case 'cancel.subscription':
+//         await this.emailService.sendEmailCancellation(dto);
+//         break;
+//       default:
+//         throw new Error('Invalid email type');
+//     }
+//     return { message: 'Email sent' };
+//   }
+// }
--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { EmailService } from './email.service';
-import { EmailController } from './email.controller';
 
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [EmailController],
+  controllers: [],
   providers: [EmailService],
   exports: [EmailService],
 })
