9e3459cde7480d7cfe393102b24cf9d66e1ecd1d
Alejandro Luna <alejandro2002.uar@gmail.com>
2025-03-13 10:38:45 -0300

    refactor

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,4 +1,209 @@
-import { BadRequestException, Body, Controller, Delete, Get, NotFoundException, Param, Post, Put, Query, UseGuards } from '@nestjs/common';
+// import { BadRequestException, Body, Controller, Delete, Get, NotFoundException, Param, Post, Put, Query, UseGuards } from '@nestjs/common';
+// import { AdminService } from './admin.service';
+// import { ApiOperation } from '@nestjs/swagger';
+// import { hash } from 'bcrypt';
+// import { Role } from 'src/auth/roles.enum';
+// import { Repository } from 'typeorm';
+// import { InjectRepository } from '@nestjs/typeorm';
+// import { User } from 'src/users/entities/user.entity';
+// import { AuthenticationGuard } from 'src/auth/guards/auth.guard';
+// import { RolesGuard } from 'src/auth/guards/roles.guard';
+// import { Roles } from 'src/auth/roles.decorator';
+// import { subsType } from 'src/users/enum/suscriptionType';
+// import { OrderDetail } from 'src/order_detail/entities/order_detail.entity';
+// import { updateDTO } from 'src/auth/dto/updated.dto';
+
+// @Controller('admin')
+// export class AdminController {
+//   constructor(
+//     private readonly adminService: AdminService,
+//     @InjectRepository(User) private readonly userRepository: Repository<User>,
+//     @InjectRepository(OrderDetail) private readonly orderDetailRepository: Repository<OrderDetail>
+//   ) {}
+
+
+//    // Crear un usuario admin por default 
+//     @ApiOperation({
+//       summary: 'Crear un usuario admin por default -> email: admin@gmail.com, password: admin'
+//     })
+//     @Post()
+//     async createAdmin() {
+//       const userExists = await this.userRepository.findOneBy({ email: 'admin@gmail.com' });
+    
+//       if (userExists) {
+//         throw new BadRequestException('El usuario admin ya existe');
+//       }
+    
+//       const user = new User();
+//       user.name = 'admin';
+//       user.email = 'admin@gmail.com';
+//       user.password = await hash('admin', 10);
+//       user.isAdmin = true;
+    
+//       return await this.userRepository.save(user);
+//     }
+    
+
+//     // Un usuario por id
+//     @Roles(Role.ADMIN)
+//     @UseGuards(AuthenticationGuard, RolesGuard)
+//     @ApiOperation({
+//       summary: 'Obtener un usuario por id'
+//     })
+//     @Get(':id')
+//     async findById(@Param('id') id: string) {
+//         return await this.adminService.findById(id)
+//     }
+
+//     // Actualizar un usuario
+//     @Roles(Role.ADMIN)
+//     @UseGuards(AuthenticationGuard, RolesGuard)
+//     @ApiOperation({
+//       summary: 'Actualizar un usuario'
+//     })
+//     @Put(':id')
+//     async updateUser(
+//       @Body() data: updateDTO, 
+//       @Param('id') id: string) {
+//       return await this.adminService.updateUser(id, data)
+//     }
+
+//     // Eliminar un usuario
+//     @Roles(Role.ADMIN)
+//     @UseGuards(AuthenticationGuard, RolesGuard)
+//     @ApiOperation({
+//       summary: 'Eliminar un usuario'
+//     })
+//     @Delete(':id')
+//     async deleteUser(@Param('id') id: string) {
+//       return await this.adminService.deleteUser(id)
+//     }
+
+//     @ApiOperation({ summary: 'Obtener usuarios con suscripción free' })
+//     @Roles(Role.ADMIN)
+//     @UseGuards(AuthenticationGuard, RolesGuard)
+//     @Get('subscription/free')
+//     async getFreeUsers(): Promise<User[]> {
+//       return this.adminService.findFreeUsers();
+//     }
+
+//     @ApiOperation({ summary: 'Obtener usuarios con suscripción premium' })
+//     @Roles(Role.ADMIN)
+//     @UseGuards(AuthenticationGuard, RolesGuard)
+//     @Get('subscription/premium')
+//     async getPremiumUsers(): Promise<User[]> {
+//       return this.adminService.findPremiumUsers();
+//     }
+
+//     @ApiOperation({ summary: 'Obtener usuarios con suscripción pro' })
+//     @Roles(Role.ADMIN)
+//     @UseGuards(AuthenticationGuard, RolesGuard)
+//     @Get('subscription/pro')
+//     async getProUsers(): Promise<User[]> {
+//       return this.adminService.findProUsers();
+//     }
+
+//     @Get('banned/users')
+//     @Roles(Role.ADMIN)
+//     @UseGuards(AuthenticationGuard, RolesGuard)
+//     @ApiOperation({ summary: 'Obtener usuarios baneados' })
+//     async getBannedUsers(){
+//        return await this.adminService.usersBanned()
+//     }
+
+//     // @ApiOperation({ summary: 'Total de ventas' })
+//     // @Get('total/sales')
+//     // @Roles(Role.ADMIN)
+//     // @UseGuards(AuthenticationGuard, RolesGuard)
+//     // async totalSales() {
+//     //   const result = await this.orderDetailRepository
+//     //     .createQueryBuilder('orderDetail')
+//     //     .select('SUM(orderDetail.price)', 'total')
+//     //     .getRawOne();
+    
+//     //   // Si no hay resultados, devolver 0 en lugar de null
+//     //   const total = result?.total ?? 0; // Si result.total es null o undefined, devolver 0
+    
+//     //   return { total };
+//     // }
+
+//     /* Luci */
+//   @ApiOperation({ summary: 'Total de ventas' })
+//   @Get('total/sales')
+//   @Roles(Role.ADMIN)
+//   @UseGuards(AuthenticationGuard, RolesGuard)
+//   async totalSales() {
+//     const results = await this.orderDetailRepository
+//       .createQueryBuilder('orderDetail')
+//       .select([
+//         'SUM(orderDetail.price) as total',
+//         'orderDetail.startedAt as date'
+//       ])
+//       .groupBy('orderDetail.startedAt')
+//       .getRawMany();
+
+//     const total = results.reduce((sum, record) => sum + Number(record.total), 0);
+//     const orders = results.map(record => ({
+//       date: record.date,
+//       amount: Number(record.total)
+//     }));
+
+//     return { total, orders };
+//   }
+
+//   // Métricas: Usuarios registrados por mes (usando createdAt)
+//   @Roles(Role.ADMIN)
+//   @UseGuards(AuthenticationGuard, RolesGuard)
+//   @ApiOperation({ summary: 'Obtener métricas de registros mensuales' })
+//   @Get('metrics/registrations')
+//   async getMonthlyRegistrations() {
+//     const data = await this.adminService.getMonthlyRegistrations();
+//     return data;
+//   }
+
+//   // Métricas: Variación mensual de suscripciones (por tipo y mes)
+//   @Roles(Role.ADMIN)
+//   @UseGuards(AuthenticationGuard, RolesGuard)
+//   @ApiOperation({ summary: 'Obtener variación mensual de suscripciones' })
+//   @Get('metrics/subscriptions')
+//   async getMonthlySubscriptions() {
+//     const data = await this.adminService.getMonthlySubscriptionVariation();
+//     return data;
+//   }
+
+//   @ApiOperation({ summary: 'Seeder: crear 100 usuarios ficticios' })
+//   // @Roles(Role.ADMIN)
+//   // @UseGuards(AuthenticationGuard, RolesGuard)
+//   @Post('seeder/users')
+//   async seedUsers() {
+//     return await this.adminService.seedUsers();
+//   }
+
+//   @Roles(Role.ADMIN)
+//   @UseGuards(AuthenticationGuard, RolesGuard)
+//   @ApiOperation({
+//     summary: 'Obtener conteo total de usuarios'
+//   })
+//   @Get()
+//   async getUsersCount() {
+//     const count = await this.userRepository.count();
+//     return { count };
+//   }
+
+//   // Obtener el total de usuarios registrados
+//   @Roles(Role.ADMIN)
+//   @UseGuards(AuthenticationGuard, RolesGuard)
+//   @ApiOperation({
+//     summary: 'Obtener el total de usuarios registrados'
+//   })
+//   @Get('total')
+//   async findAll() {
+//     return await this.userRepository.find();
+//   }
+
+// }
+
+import { Body, Controller, Delete, Get, Put, Param, Post, UseGuards } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { ApiOperation } from '@nestjs/swagger';
 import { hash } from 'bcrypt';
@@ -9,9 +214,7 @@ import { User } from 'src/users/entities/user.entity';
 import { AuthenticationGuard } from 'src/auth/guards/auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { Roles } from 'src/auth/roles.decorator';
-import { subsType } from 'src/users/enum/suscriptionType';
 import { OrderDetail } from 'src/order_detail/entities/order_detail.entity';
-import { updateDTO } from 'src/auth/dto/updated.dto';
 
 @Controller('admin')
 export class AdminController {
@@ -21,39 +224,43 @@ export class AdminController {
     @InjectRepository(OrderDetail) private readonly orderDetailRepository: Repository<OrderDetail>
   ) {}
 
+  // Crear admin por default
+  @ApiOperation({
+    summary: 'Crear un usuario admin por default -> email: admin@gmail.com, password: admin'
+  })
+  @Post()
+  async createAdmin() {
+    const user = new User();
+    user.name = 'admin';
+    user.email = 'admin@gmail.com';
+    const passHashed = await hash('admin', 10);
+    user.password = passHashed;
+    user.isAdmin = true;
+    return await this.userRepository.save(user);
+  }
 
-   // Crear un usuario admin por default 
-    @ApiOperation({
-      summary: 'Crear un usuario admin por default -> email: admin@gmail.com, password: admin'
-    })
-    @Post()
-    async createAdmin() {
-      const userExists = await this.userRepository.findOneBy({ email: 'admin@gmail.com' });
-    
-      if (userExists) {
-        throw new BadRequestException('El usuario admin ya existe');
-      }
-    
-      const user = new User();
-      user.name = 'admin';
-      user.email = 'admin@gmail.com';
-      user.password = await hash('admin', 10);
-      user.isAdmin = true;
-    
-      return await this.userRepository.save(user);
-    }
-    
+  // Conteo total de usuarios
+  @Roles(Role.ADMIN)
+  @UseGuards(AuthenticationGuard, RolesGuard)
+  @ApiOperation({
+    summary: 'Obtener conteo total de usuarios'
+  })
+  @Get()
+  async getUsersCount() {
+    const count = await this.userRepository.count();
+    return { count };
+  }
 
-    // Un usuario por id
-    @Roles(Role.ADMIN)
-    @UseGuards(AuthenticationGuard, RolesGuard)
-    @ApiOperation({
-      summary: 'Obtener un usuario por id'
-    })
-    @Get(':id')
-    async findById(@Param('id') id: string) {
-        return await this.adminService.findById(id)
-    }
+  // Obtener el total de usuarios registrados
+  @Roles(Role.ADMIN)
+  @UseGuards(AuthenticationGuard, RolesGuard)
+  @ApiOperation({
+    summary: 'Obtener el total de usuarios registrados'
+  })
+  @Get('total')
+  async findAll() {
+    return await this.userRepository.find();
+  }
 
     // Actualizar un usuario
     @Roles(Role.ADMIN)
@@ -62,72 +269,69 @@ export class AdminController {
       summary: 'Actualizar un usuario'
     })
     @Put(':id')
-    async updateUser(
-      @Body() data: updateDTO, 
-      @Param('id') id: string) {
+    async updateUser(@Body() data: any, @Param('id') id: string) {
       return await this.adminService.updateUser(id, data)
     }
 
-    // Eliminar un usuario
-    @Roles(Role.ADMIN)
-    @UseGuards(AuthenticationGuard, RolesGuard)
-    @ApiOperation({
-      summary: 'Eliminar un usuario'
-    })
-    @Delete(':id')
-    async deleteUser(@Param('id') id: string) {
-      return await this.adminService.deleteUser(id)
-    }
+  // Obtener un usuario por id
+  @Roles(Role.ADMIN)
+  @UseGuards(AuthenticationGuard, RolesGuard)
+  @ApiOperation({
+    summary: 'Obtener un usuario por id'
+  })
+  @Get(':id')
+  async findById(@Param('id') id: string) {
+    return await this.adminService.findById(id);
+  }
 
-    @ApiOperation({ summary: 'Obtener usuarios con suscripción free' })
-    @Roles(Role.ADMIN)
-    @UseGuards(AuthenticationGuard, RolesGuard)
-    @Get('subscription/free')
-    async getFreeUsers(): Promise<User[]> {
-      return this.adminService.findFreeUsers();
-    }
+  // Eliminar un usuario (se marca como eliminado)
+  @Roles(Role.ADMIN)
+  @UseGuards(AuthenticationGuard, RolesGuard)
+  @ApiOperation({
+    summary: 'Eliminar un usuario'
+  })
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string) {
+    return await this.adminService.deleteUser(id);
+  }
 
-    @ApiOperation({ summary: 'Obtener usuarios con suscripción premium' })
-    @Roles(Role.ADMIN)
-    @UseGuards(AuthenticationGuard, RolesGuard)
-    @Get('subscription/premium')
-    async getPremiumUsers(): Promise<User[]> {
-      return this.adminService.findPremiumUsers();
-    }
+  // Usuarios con suscripción free
+  @ApiOperation({ summary: 'Obtener usuarios con suscripción free' })
+  @Roles(Role.ADMIN)
+  @UseGuards(AuthenticationGuard, RolesGuard)
+  @Get('subscription/free')
+  async getFreeUsers(): Promise<User[]> {
+    return this.adminService.findFreeUsers();
+  }
 
-    @ApiOperation({ summary: 'Obtener usuarios con suscripción pro' })
-    @Roles(Role.ADMIN)
-    @UseGuards(AuthenticationGuard, RolesGuard)
-    @Get('subscription/pro')
-    async getProUsers(): Promise<User[]> {
-      return this.adminService.findProUsers();
-    }
+  // Usuarios con suscripción premium
+  @ApiOperation({ summary: 'Obtener usuarios con suscripción premium' })
+  @Roles(Role.ADMIN)
+  @UseGuards(AuthenticationGuard, RolesGuard)
+  @Get('subscription/premium')
+  async getPremiumUsers(): Promise<User[]> {
+    return this.adminService.findPremiumUsers();
+  }
 
-    @Get('banned/users')
-    @Roles(Role.ADMIN)
-    @UseGuards(AuthenticationGuard, RolesGuard)
-    @ApiOperation({ summary: 'Obtener usuarios baneados' })
-    async getBannedUsers(){
-       return await this.adminService.usersBanned()
-    }
+  // Usuarios con suscripción pro
+  @ApiOperation({ summary: 'Obtener usuarios con suscripción pro' })
+  @Roles(Role.ADMIN)
+  @UseGuards(AuthenticationGuard, RolesGuard)
+  @Get('subscription/pro')
+  async getProUsers(): Promise<User[]> {
+    return this.adminService.findProUsers();
+  }
 
-    // @ApiOperation({ summary: 'Total de ventas' })
-    // @Get('total/sales')
-    // @Roles(Role.ADMIN)
-    // @UseGuards(AuthenticationGuard, RolesGuard)
-    // async totalSales() {
-    //   const result = await this.orderDetailRepository
-    //     .createQueryBuilder('orderDetail')
-    //     .select('SUM(orderDetail.price)', 'total')
-    //     .getRawOne();
-    
-    //   // Si no hay resultados, devolver 0 en lugar de null
-    //   const total = result?.total ?? 0; // Si result.total es null o undefined, devolver 0
-    
-    //   return { total };
-    // }
+  // Usuarios baneados
+  @Get('banned/users')
+  @Roles(Role.ADMIN)
+  @UseGuards(AuthenticationGuard, RolesGuard)
+  @ApiOperation({ summary: 'Obtener usuarios baneados' })
+  async getBannedUsers() {
+    return await this.adminService.usersBanned();
+  }
 
-    /* Luci */
+  // Total de ventas agrupadas por fecha
   @ApiOperation({ summary: 'Total de ventas' })
   @Get('total/sales')
   @Roles(Role.ADMIN)
@@ -178,29 +382,5 @@ export class AdminController {
   async seedUsers() {
     return await this.adminService.seedUsers();
   }
-
-  @Roles(Role.ADMIN)
-  @UseGuards(AuthenticationGuard, RolesGuard)
-  @ApiOperation({
-    summary: 'Obtener conteo total de usuarios'
-  })
-  @Get()
-  async getUsersCount() {
-    const count = await this.userRepository.count();
-    return { count };
-  }
-
-  // Obtener el total de usuarios registrados
-  @Roles(Role.ADMIN)
-  @UseGuards(AuthenticationGuard, RolesGuard)
-  @ApiOperation({
-    summary: 'Obtener el total de usuarios registrados'
-  })
-  @Get('total')
-  async findAll() {
-    return await this.userRepository.find();
-  }
-
 }
-
 
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,3 +1,142 @@
+// import { Injectable, NotFoundException } from '@nestjs/common';
+// import { InjectRepository } from '@nestjs/typeorm';
+// import { User } from 'src/users/entities/user.entity';
+// import { subsType } from 'src/users/enum/suscriptionType';
+// import { Repository } from 'typeorm';
+
+// @Injectable()
+// export class AdminService {
+
+//     constructor(
+//         @InjectRepository(User) private readonly userRepository: Repository<User>,
+//       ){}
+
+//     async findAll() {
+//         return this.userRepository.find({
+//           relations: {
+//             clothes: true,
+//             combinations: true
+//           }
+//      })
+//     }
+
+//     async findById(id: string) {
+//       return this.userRepository.find({
+//         where: {id},
+//         relations: {
+//           clothes: true,
+//           combinations: true
+//         }
+//      })
+//   }
+
+//   async updateUser(id: string, data: Partial<User>) {
+//     const user = await this.userRepository.findOneBy({id: id})
+//     if (!user) throw new NotFoundException('Usuario no encontrado');
+//     await this.userRepository.update(user.id, { ...data })
+//     return this.userRepository.findOneBy({ id: user.id });
+//   }
+
+//   async deleteUser(id: string) {
+//     const foundUser = await this.userRepository.findOne({where: {id: id}})
+//     if (!foundUser) {
+//       throw new NotFoundException('User not found')
+//     }
+//     foundUser.isDeleted = true
+//     await this.userRepository.save(foundUser)
+//     return `User with id: ${id} has been removed`
+//   }
+
+//   async findFreeUsers(){
+//     return await this.userRepository.find({
+//       where: {
+//         subscriptionType: subsType.free
+//       }
+//     })
+//   }
+
+//   async findPremiumUsers(){
+//     return await this.userRepository.find({
+//       where: {
+//         subscriptionType: subsType.premium
+//       }
+//     })
+//   }
+
+//   async findProUsers(){
+//     return await this.userRepository.find({
+//       where: {
+//         subscriptionType: subsType.pro
+//       }
+//     })
+//   }
+
+//  async usersBanned(){
+//     return await this.userRepository.find({
+//       where:{
+//         isDeleted: true
+//       }
+//     })
+//  }
+
+
+//  // Luci
+// // Obtiene la cantidad de usuarios registrados por mes (formato YYYY-MM)
+//   async getMonthlyRegistrations() {
+//     return await this.userRepository
+//       .createQueryBuilder("user")
+//       .select("TO_CHAR(user.registeredAt, 'YYYY-MM')", "month")
+//       .addSelect("COUNT(user.id)", "count")
+//       .groupBy("month")
+//       .orderBy("month", "ASC")
+//       .getRawMany();
+//   }
+
+// // Obtiene la variación mensual de suscripciones por tipo
+//   async getMonthlySubscriptionVariation() {
+//     return await this.userRepository
+//       .createQueryBuilder("user")
+//       .select("TO_CHAR(user.registeredAt, 'YYYY-MM')", "month")
+//       .addSelect("user.subscriptionType", "subscriptionType")
+//       .addSelect("COUNT(user.id)", "count")
+//       .groupBy("month")
+//       .addGroupBy("user.subscriptionType")
+//       .orderBy("month", "ASC")
+//       .getRawMany();
+//   }
+
+//   /**
+//  * Seeder: Crea 100 usuarios ficticios con fechas de registro y tipos de suscripción aleatorios.
+//  */
+//   async seedUsers(): Promise<{ message: string }> {
+//     const subscriptionTypes = [subsType.free, subsType.premium, subsType.pro];
+//     const currentYear = new Date().getFullYear();
+//     const startDate = new Date(`${currentYear}-01-01`);
+//     const endDate = new Date(`${currentYear}-12-31`);
+
+//     // Función auxiliar para generar una fecha aleatoria entre dos fechas
+//     const randomDate = (start: Date, end: Date): Date => {
+//       return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+//     };
+
+//     const usersToCreate = 100;
+//     for (let i = 0; i < usersToCreate; i++) {
+//       const user = this.userRepository.create({
+//         name: `User${i}`,
+//         email: `user${i}@example.com`,
+//         password: 'password', // En producción deberías encriptarla
+//         registeredAt: randomDate(startDate, endDate), // Asignar una fecha aleatoria para el registro
+//         isAdmin: false,
+//         subscriptionType: subscriptionTypes[Math.floor(Math.random() * subscriptionTypes.length)],
+//         isDeleted: false,
+//       });
+//       await this.userRepository.save(user);
+//     }
+//     return { message: '100 usuarios ficticios creados exitosamente.' };
+//   }
+// }
+
+
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
@@ -6,28 +145,37 @@ import { Repository } from 'typeorm';
 
 @Injectable()
 export class AdminService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
 
-    constructor(
-        @InjectRepository(User) private readonly userRepository: Repository<User>,
-      ){}
+  async findAll() {
+    return this.userRepository.find({
+      relations: {
+        clothes: true,
+        combinations: true
+      }
+    });
+  }
 
-    async findAll() {
-        return this.userRepository.find({
-          relations: {
-            clothes: true,
-            combinations: true
-          }
-     })
+  async findById(id: string) {
+    return this.userRepository.find({
+      where: { id },
+      relations: {
+        clothes: true,
+        combinations: true
+      }
+    });
+  }
+
+  async deleteUser(id: string) {
+    const foundUser = await this.userRepository.findOne({ where: { id } });
+    if (!foundUser) {
+      throw new NotFoundException('User not found');
     }
-
-    async findById(id: string) {
-      return this.userRepository.find({
-        where: {id},
-        relations: {
-          clothes: true,
-          combinations: true
-        }
-     })
+    foundUser.isDeleted = true;
+    await this.userRepository.save(foundUser);
+    return `User with id: ${id} has been removed`;
   }
 
   async updateUser(id: string, data: Partial<User>) {
@@ -37,51 +185,31 @@ export class AdminService {
     return this.userRepository.findOneBy({ id: user.id });
   }
 
-  async deleteUser(id: string) {
-    const foundUser = await this.userRepository.findOne({where: {id: id}})
-    if (!foundUser) {
-      throw new NotFoundException('User not found')
-    }
-    foundUser.isDeleted = true
-    await this.userRepository.save(foundUser)
-    return `User with id: ${id} has been removed`
+  async findFreeUsers() {
+    return await this.userRepository.find({
+      where: { subscriptionType: subsType.free }
+    });
   }
 
-  async findFreeUsers(){
+  async findPremiumUsers() {
     return await this.userRepository.find({
-      where: {
-        subscriptionType: subsType.free
-      }
-    })
+      where: { subscriptionType: subsType.premium }
+    });
   }
 
-  async findPremiumUsers(){
+  async findProUsers() {
     return await this.userRepository.find({
-      where: {
-        subscriptionType: subsType.premium
-      }
-    })
+      where: { subscriptionType: subsType.pro }
+    });
   }
 
-  async findProUsers(){
+  async usersBanned() {
     return await this.userRepository.find({
-      where: {
-        subscriptionType: subsType.pro
-      }
-    })
+      where: { isDeleted: true }
+    });
   }
 
- async usersBanned(){
-    return await this.userRepository.find({
-      where:{
-        isDeleted: true
-      }
-    })
- }
-
-
- // Luci
-// Obtiene la cantidad de usuarios registrados por mes (formato YYYY-MM)
+  // Obtiene la cantidad de usuarios registrados por mes (formato YYYY-MM)
   async getMonthlyRegistrations() {
     return await this.userRepository
       .createQueryBuilder("user")
@@ -92,7 +220,7 @@ export class AdminService {
       .getRawMany();
   }
 
-// Obtiene la variación mensual de suscripciones por tipo
+  // Obtiene la variación mensual de suscripciones por tipo
   async getMonthlySubscriptionVariation() {
     return await this.userRepository
       .createQueryBuilder("user")
@@ -105,35 +233,33 @@ export class AdminService {
       .getRawMany();
   }
 
-  /**
- * Seeder: Crea 100 usuarios ficticios con fechas de registro y tipos de suscripción aleatorios.
- */
-  async seedUsers(): Promise<{ message: string }> {
-    const subscriptionTypes = [subsType.free, subsType.premium, subsType.pro];
-    const currentYear = new Date().getFullYear();
-    const startDate = new Date(`${currentYear}-01-01`);
-    const endDate = new Date(`${currentYear}-12-31`);
-
-    // Función auxiliar para generar una fecha aleatoria entre dos fechas
-    const randomDate = (start: Date, end: Date): Date => {
-      return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
-    };
-
-    const usersToCreate = 100;
-    for (let i = 0; i < usersToCreate; i++) {
-      const user = this.userRepository.create({
-        name: `User${i}`,
-        email: `user${i}@example.com`,
-        password: 'password', // En producción deberías encriptarla
-        registeredAt: randomDate(startDate, endDate), // Asignar una fecha aleatoria para el registro
-        isAdmin: false,
-        subscriptionType: subscriptionTypes[Math.floor(Math.random() * subscriptionTypes.length)],
-        isDeleted: false,
-      });
-      await this.userRepository.save(user);
+    /**
+   * Seeder: Crea 100 usuarios ficticios con fechas de registro y tipos de suscripción aleatorios.
+   */
+    async seedUsers(): Promise<{ message: string }> {
+      const subscriptionTypes = [subsType.free, subsType.premium, subsType.pro];
+      const currentYear = new Date().getFullYear();
+      const startDate = new Date(`${currentYear}-01-01`);
+      const endDate = new Date(`${currentYear}-12-31`);
+  
+      // Función auxiliar para generar una fecha aleatoria entre dos fechas
+      const randomDate = (start: Date, end: Date): Date => {
+        return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+      };
+  
+      const usersToCreate = 100;
+      for (let i = 0; i < usersToCreate; i++) {
+        const user = this.userRepository.create({
+          name: `User${i}`,
+          email: `user${i}@example.com`,
+          password: 'password',
+          registeredAt: randomDate(startDate, endDate), // Asignar una fecha aleatoria para el registro
+          isAdmin: false,
+          subscriptionType: subscriptionTypes[Math.floor(Math.random() * subscriptionTypes.length)],
+          isDeleted: false,
+        });
+        await this.userRepository.save(user);
+      }
+      return { message: '100 usuarios ficticios creados exitosamente.' };
     }
-    return { message: '100 usuarios ficticios creados exitosamente.' };
-  }
 }
-
-
